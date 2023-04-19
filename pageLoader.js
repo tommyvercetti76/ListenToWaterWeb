7ad6442557dc3123c5774d00e45a9a4b1a7e6d30
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-19 14:31:49 -0700

    Animation Added to Page opening

--- pageLoader.js
+++ pageLoader.js
@@ -1,9 +1,16 @@
+function toggleMenu() {
+  const menuItems = document.getElementById("menu-items");
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  menuItems.classList.toggle("open");
+  hamburgerMenu.classList.toggle("open");
+}
+
 function openPage(url) {
   const contentIframe = document.getElementById("content-iframe");
   const closeBtn = document.getElementById("close-btn");
 
   contentIframe.src = url;
-  contentIframe.classList.add('visible');
+  contentIframe.classList.add("visible");
   closeBtn.style.display = "block";
 }
 
@@ -12,6 +19,23 @@ function closePage() {
   const closeBtn = document.getElementById("close-btn");
 
   contentIframe.src = "";
-  contentIframe.classList.remove('visible');
+  contentIframe.classList.remove("visible");
   closeBtn.style.display = "none";
 }
+
+function isMobile() {
+  return window.innerWidth <= 640;
+}
+
+document.querySelectorAll(".menu-items a").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    toggleMenu();
+
+    if (isMobile()) {
+      openPage(event.target.href);
+    } else {
+      window.location.href = event.target.href;
+    }
+  });
+});
