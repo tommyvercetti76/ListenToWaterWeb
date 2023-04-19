5b3bf8961071a81a6ef37e670ffc29f7fd024bc6
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-18 21:00:04 -0700

    File Updates

--- menu.js
+++ menu.js
@@ -1,9 +1,41 @@
-document.querySelectorAll(".menu-items li a").forEach((link) =>
-  link.addEventListener("click", (event) => {
+function toggleMenu() {
+  const menuItems = document.getElementById('menu-items');
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  menuItems.classList.toggle('open');
+  hamburgerMenu.classList.toggle('open');
+}
+
+function isMobile() {
+  return window.innerWidth <= 640;
+}
+
+function openPage(url) {
+  const contentIframe = document.getElementById("content-iframe");
+  const closeBtn = document.getElementById("close-btn");
+
+  contentIframe.src = url;
+  contentIframe.style.display = "block";
+  closeBtn.style.display = "block";
+}
+
+function closePage() {
+  const contentIframe = document.getElementById("content-iframe");
+  const closeBtn = document.getElementById("close-btn");
+
+  contentIframe.src = "";
+  contentIframe.style.display = "none";
+  closeBtn.style.display = "none";
+}
+
+document.querySelectorAll('.menu-items a').forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    toggleMenu();
+
     if (isMobile()) {
-      event.preventDefault();
-      closeMenu();
-      openPage(link.href);
+      openPage(event.target.href);
+    } else {
+      window.location.href = event.target.href;
     }
-  })
-);
+  });
+});
