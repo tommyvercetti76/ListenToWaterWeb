6855de7eb370bfcb8269093784118ddbaf6ccfbe
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-18 21:28:21 -0700

    File Updates

--- menu.js
+++ menu.js
@@ -1,41 +1,45 @@
 const menuItems = document.getElementById("menu-items");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
-const contentIframe = document.getElementById("content-iframe");
-const closeBtn = document.getElementById("close-btn");
+const pageContainer = document.querySelector(".page-container");
+const closePage = document.querySelector(".close-page");
 
 function toggleMenu() {
   menuItems.classList.toggle("open");
   hamburgerMenu.classList.toggle("open");
 }
 
-function openPage(e, url) {
-  e.preventDefault();
+function openPage(url) {
   if (window.innerWidth <= 768) {
-    toggleMenu();
-    hamburgerMenu.style.display = "none";
-    contentIframe.src = url;
-    contentIframe.style.display = "block";
-    closeBtn.style.display = "block";
+    hamburgerMenu.classList.add("mobile-hidden");
+    pageContainer.classList.add("open");
+    document.querySelector(".content-iframe").src = url;
+    document.querySelector(".content-iframe").style.display = "block";
+    document.querySelector(".close-btn").style.display = "block";
   } else {
     window.location.href = url;
   }
 }
 
-function closePage() {
-  hamburgerMenu.style.display = "block";
-  contentIframe.style.display = "none";
-  closeBtn.style.display = "none";
+function closePageHandler() {
+  hamburgerMenu.classList.remove("mobile-hidden");
+  pageContainer.classList.remove("open");
+  document.querySelector(".content-iframe").style.display = "none";
+  document.querySelector(".close-btn").style.display = "none";
 }
 
 hamburgerMenu.addEventListener("click", toggleMenu);
-closeBtn.addEventListener("click", closePage);
+closePage.addEventListener("click", closePageHandler);
 
 document.querySelectorAll(".menu-items li a").forEach((menuItem) => {
-  menuItem.addEventListener("click", (e) => openPage(e, menuItem.getAttribute("href")));
+  menuItem.addEventListener("click", (e) => {
+    e.preventDefault();
+    openPage(menuItem.getAttribute("href"));
+    toggleMenu();
+  });
 });
 
 window.addEventListener("resize", () => {
   if (window.innerWidth > 768) {
-    hamburgerMenu.style.display = "block";
+    hamburgerMenu.classList.remove("mobile-hidden");
   }
 });
