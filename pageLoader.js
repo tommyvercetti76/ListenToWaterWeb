195a56e701b8bb73e619ccc8626516225d72ce64
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-18 20:45:27 -0700

    file updates

---
+++ pageLoader.js
@@ -0,0 +1,28 @@
+function openPage(url) {
+    fetch(url)
+      .then((response) => response.text())
+      .then((html) => {
+        document.getElementById("page-content").innerHTML = html;
+        const pageContainer = document.getElementById("page-container");
+        pageContainer.classList.add("open");
+      })
+      .catch((error) => {
+        console.warn(`Error fetching page: ${url}`, error);
+      });
+  }
+  
+  function closePage() {
+    const pageContainer = document.getElementById("page-container");
+    pageContainer.classList.remove("open");
+  }
+  
+  document
+    .querySelectorAll(".menu-items li a")
+    .forEach((link) =>
+      link.addEventListener("click", (event) => {
+        event.preventDefault();
+        closeMenu();
+        openPage(link.getAttribute("href"));
+      })
+    );
+  
