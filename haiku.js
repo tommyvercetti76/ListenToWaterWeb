0013a1f3380ff053eaf07ca3747e0e8a05cf3319
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-20 11:11:37 -0700

    Haiku Added

--- haiku.js
+++ haiku.js
@@ -1,48 +1,58 @@
-const line1Words = {
-    morning: ['Gentle morning dew', 'Sunlit sparkling stream', 'Fresh dawn by the creek'],
-    afternoon: ['Rays on the river', 'A glistening cascade', 'Bright noon at the lake'],
-    evening: ['Calm twilight ripples', 'Dusk embraces the pond', 'Soft waves on the shore']
-};
-
-const line2Words = {
-    morning: ['Brings life to the earth', 'Awakens the senses', 'Revives the weary soul'],
-    afternoon: ['Quenches the parched ground', 'Eases the burdened mind', 'Soothes the anxious heart'],
-    evening: ['Reflects the moonlight', 'Whispers to the night breeze', 'Serenades the stars']
-};
-
-const line3Words = {
-    morning: ['Nature sings anew', 'Hope dances in the air', 'Life blossoms once more'],
-    afternoon: ['Peaceful harmony', 'A moment of respite', 'Stillness in the sun'],
-    evening: ['Restful lullaby', 'A quiet symphony', 'Dreams merge with the sky']
-};
-
-function getTimeOfDay() {
-    const currentHour = new Date().getHours();
-
-    if (currentHour >= 5 && currentHour < 12) {
-        return 'morning';
-    } else if (currentHour >= 12 && currentHour < 18) {
-        return 'afternoon';
-    } else {
-        return 'evening';
-    }
-}
-
-function getRandomElement(arr) {
+const timeOfDayPhrases = [
+    "morning dewdrops",
+    "noon's gentle stream",
+    "evening's quiet rain",
+    "night's calm river",
+    "dawn's misty veil",
+    "twilight's soft murmur",
+    "sunrise's shimmer",
+    "sunset's reflection",
+    "daybreak's golden light",
+    "dusk's fading glow",
+    "midday's warm embrace"
+  ];
+  
+  const soundPhrases = [
+    "whisper to the rocks",
+    "ripple through the reeds",
+    "splash against the shore",
+    "echo in the canyon",
+    "tinkle on the leaves",
+    "bubble in the creek",
+    "gurgle in the brook",
+    "patter on the roof",
+    "chatter in the breeze",
+    "trickle over stones",
+    "drum on the earth"
+  ];
+  
+  const benefitPhrases = [
+    "soothe the restless mind",
+    "quench the thirst of life",
+    "nurture the seeds of hope",
+    "purify the soul",
+    "calm the weary heart",
+    "awaken inner peace",
+    "renew the spirit",
+    "inspire the dreamer",
+    "heal the wounded",
+    "uplift the burdened",
+    "rekindle the flame"
+  ];
+  
+  function getRandomElement(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
-}
-
-function generateHaiku() {
-    const timeOfDay = getTimeOfDay();
-
-    const line1 = getRandomElement(line1Words[timeOfDay]);
-    const line2 = getRandomElement(line2Words[timeOfDay]);
-    const line3 = getRandomElement(line3Words[timeOfDay]);
-
-    return `${line1}\n${line2}\n${line3}`;
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    const haikuElement = document.querySelector('.haiku');
+  }
+  
+  function generateHaiku() {
+    const timeOfDay = getRandomElement(timeOfDayPhrases);
+    const sound = getRandomElement(soundPhrases);
+    const benefit = getRandomElement(benefitPhrases);
+    return `${timeOfDay}\n${sound}\n${benefit}`;
+  }
+  
+  document.addEventListener("DOMContentLoaded", () => {
+    const haikuElement = document.querySelector(".haiku");
     haikuElement.textContent = generateHaiku();
-});
+  });
+  
