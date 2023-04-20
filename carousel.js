1f19fbf2af22181f7793613ff671a199aa1ba364
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-20 10:30:40 -0700

    Carousel Possibly stable

--- carousel.js
+++ carousel.js
@@ -60,3 +60,19 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 });
 
+let slideIndex = 0;
+let slides = document.getElementsByClassName("slide");
+
+function showSlides() {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change slides every 4 seconds
+}
+
+showSlides();
