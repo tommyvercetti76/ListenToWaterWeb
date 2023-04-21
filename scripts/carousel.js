487ff5c16b52a6e6b3ab3cb59073f94c9d95fcd5
Rohan Ramekar <rohanramekar17@gmail.com>
2023-04-20 19:30:41 -0700

    Refactor

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,22 +1,5 @@
-let touchStartX = null;
-let touchEndX = null;
 let slideIndex = 0;
 let slides = document.getElementsByClassName("slide");
-
-function handleTouchStart(event) {
-  touchStartX = event.touches[0].clientX;
-}
-
-function handleTouchEnd(event) {
-  touchEndX = event.changedTouches[0].clientX;
-  if (touchStartX && touchEndX) {
-    if (touchEndX - touchStartX > 100) {
-      previousSlide();
-    } else if (touchStartX - touchEndX > 100) {
-      nextSlide();
-    }
-  }
-}
 
 function setActiveSlide(index) {
   const activeSlide = document.querySelector('.slide.active');
@@ -31,25 +14,7 @@ function setActiveSlide(index) {
   slides[index].classList.add('active');
 }
 
-function nextSlide() {
-  const activeSlide = document.querySelector('.slide.active');
-  const activeIndex = Array.from(slides).indexOf(activeSlide);
-  const nextIndex = (activeIndex + 1) % slides.length;
-  setActiveSlide(nextIndex);
-}
-
-function previousSlide() {
-  const activeSlide = document.querySelector('.slide.active');
-  const activeIndex = Array.from(slides).indexOf(activeSlide);
-  const prevIndex = (activeIndex - 1 + slides.length) % slides.length;
-  setActiveSlide(prevIndex);
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-  const carousel = document.querySelector('.carousel');
-  carousel.addEventListener('touchstart', handleTouchStart, false);
-  carousel.addEventListener('touchend', handleTouchEnd, false);
-
   if (slides.length > 0) {
     setActiveSlide(0);
     showSlides();
