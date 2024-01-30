e25d62c7e6bdead3cbeb850ae6a9af816089d2ed
tommyvercetti76 <tommyvercetti76@github.com>
2024-01-30 14:24:13 -0600

    Updated Files

--- scripts/poscript.js
+++ scripts/poscript.js
@@ -9,12 +9,12 @@ fetch('https://firebasestorage.googleapis.com/v0/b/listentowaterios.appspot.com/
         // Create card HTML
         const cardHTML = `
             <div class="card">
-                <img class="card-image" src="${card.headerImgURL}" alt="${card.title}">
+                <img class="card-image" src="${card.headerImgURL}" alt="${card.title}" loading="lazy">
                 <div class="card-content">
                     <h2 class="card-title">${card.title}</h2>
-                    <p class="card-subtitle">${card.location}</p>
+                    <p class="card-location">${card.location}</p>
                     <p class="card-description">${card.description}</p>
-                    <a href="${card.youtubeURL}" target="_blank">Watch on YouTube</a>
+                    <a href="${card.youtubeURL}" target="_blank" class="youtube-link">Watch on YouTube</a>
                 </div>
             </div>
         `;
